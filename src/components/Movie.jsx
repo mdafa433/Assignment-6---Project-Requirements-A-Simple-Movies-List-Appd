fb46fd8d45a1b2bd43f62247ea0625bb00be03a3
Alex <muhammaddafa6279@gmail.com>
2024-11-21 08:30:41 +0700

    first commit

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -5,7 +5,7 @@ function MovieList({ movies }) {
     <div className="grid grid-cols-2 sm:grid-cols-5 gap-4 mt-4">
       {movies.map((movie) => (
         <div key={movie.imdbID} className="bg-orange-500 rounded shadow-lg overflow-hidden">
-          <img src={movie.Poster} alt={movie.Title} className="w-full h-96 object-cover"/>
+          <img src={movie.Poster} alt={movie.Title} className="w-full h-52 sm:h-96 object-cover"/>
           <div className="p-2 text-center">
             <h3 className="text-sm font-bold text-white">{movie.Title}</h3>
           </div>
